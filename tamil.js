cdd05b49758859192545a14eaa8e9346a03fce0b
Rethna Ganesh <rethna2@gmail.com>
2020-04-28 11:30:30 +0530

    Create tamil.js

---
+++ tamil.js
@@ -0,0 +1,55 @@
+const vowels = "அஆஇஈஉஊஎஏஐஒஓஔ".split('');
+const extra = "ஃ"
+const basic = "கஙசஞடணதநபமயரலவழளறன".split('')
+const partialsStr= 'க்காகிகீகுகூகெகேகைகொகோகௌ';
+  
+const partials = partialsStr.split('').filter(function (char, index) {
+  return index % 2 === 1;
+});
+
+
+export const getAllChars = () => {
+  const others = new Array();
+  for(let i = 0; i < basic.length; i++){
+    for(let j = 0; j < partials.length; j++){
+      others.push(basic[i] + partials[j])
+    }
+  }
+  return [
+    ...vowels,
+    extra,
+    ...basic,
+    ...others
+  ]
+}
+
+export const getTamilWordArr = str => {
+  str = str.split("").map(char => char.trim()).join('');
+  var len = 0;
+  var wordArr = [];
+  var isPrevLetterJoinable = false;
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charAt(i);
+    if (vowels.indexOf(char) != -1) {
+      len++;
+      wordArr.push(str.charAt(i));
+      isPrevLetterJoinable = false;
+    } else if (basic.indexOf(char) != -1) {
+      len++;
+      wordArr.push(str.charAt(i));
+      isPrevLetterJoinable = true;
+    } else if (partials.indexOf(char) != -1) {
+      if (isPrevLetterJoinable) {
+        isPrevLetterJoinable = false;
+        var lastIndex = wordArr.length - 1;
+        wordArr[lastIndex] = wordArr[lastIndex] + str.charAt(i);
+      } else {
+        return false;
+      }
+    } else {
+      //return false;
+      throw "invalid";
+    }
+  }
+  return wordArr;
+};
